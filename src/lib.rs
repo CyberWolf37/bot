27f498486ec1b2020e11be66a07b555ac3e07e71
CyberWolf37 <borel.loic.37@gmail.com>
2021-02-17 15:01:10 +0100

    add some change in static

--- src/lib.rs
+++ src/lib.rs
@@ -109,8 +109,8 @@ impl BotMessenger {
         self
     }
 
-    pub fn with_static_file(mut self, file: String) -> Self{
-        self.static_file = Some(file);
+    pub fn with_static_file(mut self, file: &str) -> Self{
+        self.static_file = Some(file.to_string());
         self
     }
 
@@ -139,9 +139,8 @@ impl BotMessenger {
                     .mount("/", routes![get_basic]);
 
                 if self.static_file.is_some() {
-                    let option = Options::None;
                     let s = self.static_file.clone().unwrap();
-                    rocket = rocket.mount("/static", StaticFiles::from(format!("{}{}",env!("CARGO_MANIFEST_DIR"), s.as_str())));
+                    rocket = rocket.mount("/static", StaticFiles::from(s.as_str()));
                 }
                 
                 rocket.launch();
